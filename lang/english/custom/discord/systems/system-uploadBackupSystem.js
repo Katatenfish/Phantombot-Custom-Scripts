cac269825bd3cf48d411d59f04b02e32c664ae9c
Dakoda Jackson <wayne6324@gmail.com>
2019-02-23 22:58:29 +0000

    Added Lang file for Discord Script

---
+++ lang/english/custom/discord/systems/system-uploadBackupSystem.js
@@ -0,0 +1,7 @@
+/**
+ * system-uploadBackupSystem.js
+ *
+ * Command handler to upload a backup of the database to discord.
+ */
+$.lang.register('autobackup.useage', '$1, Useage: !goinglive channel [channel].');
+$.lang.register('autobackup.pass', '$1, Going live messages will now be posted to $2.');
