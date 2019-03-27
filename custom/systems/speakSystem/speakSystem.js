d9da4d751527cc6a374fa308c3c25d94820f7b33
Dakoda Jackson <wayne6324@gmail.com>
2019-03-27 23:50:30 +0000

    Newer Version

--- custom/systems/speakSystem/speakSystem.js
+++ custom/systems/speakSystem/speakSystem.js
@@ -1,37 +1,23 @@
-//Create text to speech object.
-var TTS = {
-    speak: function(inputText){//Define function getting & playing TTS
-	var speechText = "<voice>" + inputText;
-	//Post speech text.
-	//$.say(speechText);
-	$.panelsocketserver.alertImage(speechText);
-    }
-};
 //Adding text to speech to PhantomBot
 (function(){
-    //Define function calling tts.
-    function speak(textToSpeak){
-	TTS.speak(textToSpeak);
-    }
     
     /**
      * @event command
      */
     $.bind('command',function(event){
-    	//Define cost of shouting and current amount of currency:
 	    var sender = event.getSender().toLowerCase(),
             command = event.getCommand(),
-	    	concatedArgs = "",
 	    	allArgs = event.getArgs();
 
-	    concatedArgs +=  sender + ": ";
+	    var speechText =  sender + ": ";
         for (i = 0; i < allArgs.length; i++){
-            concatedArgs += allArgs[i].replace(",", "") + " ";
+            speechText += allArgs[i].replace(",", "") + " ";
         }
-	    inputText = concatedArgs;
 
     	if (command.equalsIgnoreCase('tts')){
-			speak(inputText);
+			if (allArgs.length > 0) {
+				$.panelsocketserver.triggerTextToSpeek(speechText);
+			}
 		}
     });
 
